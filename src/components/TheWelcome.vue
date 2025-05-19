0f0670fb292cc9ca154a5bf4b32f734e05afd7ca
lankavijay <lankavijaykumar2017@gmail.com>
2025-05-19 22:13:52 +0530

    tool

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -25,7 +25,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Tools</template>
 
     This project is served and bundled with
     <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
